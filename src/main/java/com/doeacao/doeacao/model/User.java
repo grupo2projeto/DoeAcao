80e7181d4ee4987aafa0a7c9569e6186499af52a
grupo2projeto <grupo2projetointegrador@hotmail.com>
2023-10-20 13:37:58 -0300

    Update User.java

--- src/main/java/com/doeacao/doeacao/model/User.java
+++ src/main/java/com/doeacao/doeacao/model/User.java
@@ -12,21 +12,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.List;
 
-
-import jakarta.persistence.*;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
